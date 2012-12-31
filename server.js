93d8db48b2953618f775ca2272c33eba43a271ee
Kyle DeTella <kdetella@gmail.com>
2012-12-31 15:34:12 -0600

    Changed path in server.js to static files

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.configure(function(){
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
-  app.use(express.static(path.join(__dirname, 'app/public')));
-  // app.use(express.static('./app/public'));
+  // app.use(express.static(path.join(__dirname, 'app/public')));
+  app.use(express.static('./app/public/'));
 });
 
 app.configure('development', function(){
